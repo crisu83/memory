cfde1a9db594d4dc8140955ccba7ea62774eb7b8
Crisu83 <ChristofferNiska@gmail.com>
2014-01-27 02:04:50 +0200

    Refactor code and enlarge cards

--- js/memory.js
+++ js/memory.js
@@ -2,8 +2,91 @@
 
     'use strict';
 
-    var CARDS_PER_ROW = 4,
-        CARDS_PER_COLUMN = 4;
+    /**
+     * Shuffles the given array using "Fisher-Yates".
+     * @param {Array} array
+     * @returns {Array}
+     */
+    var shuffle = function (array) {
+        var counter = array.length,
+            temp, index;
+
+        while (counter > 0) {
+            index = Math.floor(Math.random() * counter);
+            counter--;
+            temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+        }
+
+        return array;
+    };
+
+    /**
+     * Creates a new collection.
+     * @constructor
+     */
+    var Collection = function (items) {
+        this.items = items || [];
+    };
+
+    /**
+     * Adds an item to this collection.
+     * @param {*} item
+     */
+    Collection.prototype.add = function (item) {
+        this.items.push(item);
+    };
+
+    /**
+     * Returns all the items in this collection.
+     * @returns {Array}
+     */
+    Collection.prototype.getItems = function () {
+        return this.items;
+    };
+
+    /**
+     * Returns a specific item in this collection.
+     * @param {Number} index
+     * @returns {*}
+     */
+    Collection.prototype.get = function (index) {
+        return this.items[index];
+    };
+
+    /**
+     * Removes a specific item from this collection.
+     * @param {*} item
+     */
+    Collection.prototype.remove = function (item) {
+        for (var i = 0; i < this.items.length; i++) {
+            if (this.items[i] === item) {
+                this.items.splice(i, 1);
+            }
+        }
+    };
+
+    /**
+     * Removes all items from this collection.
+     */
+    Collection.prototype.clear = function () {
+        this.items.length = 0;
+    };
+
+    /**
+     * Returns the number of items in this collection.
+     * @returns {Number}
+     */
+    Collection.prototype.count = function () {
+        return this.items.length;
+    };
+
+    var globals = {
+        flippedCards: new Collection(),
+        collectedCards: new Collection(),
+        board: null
+    };
 
     /**
      * Creates a new board.
@@ -13,15 +96,18 @@
      * @constructor
      */
     var Board = function (rows, cols, game) {
+        this.states = {
+            DEFAULT: 'default',
+            LOCKED: 'locked'
+        };
+
         this.rows = rows;
         this.cols = cols;
         this.game = game;
-        this.cards = [];
-        this.flippedCards = [];
-        this.collectedCards = [];
+        this.numCardsTotal = 0;
         this.score = 0;
         this.comboLength = 0;
-        this.disabled = false;
+        this.state = this.states.DEFAULT;
 
         this.init();
     };
@@ -30,21 +116,6 @@
      * Initializes the board.
      */
     Board.prototype.init = function () {
-        var shuffle = function (array) {
-            var counter = array.length,
-                temp, index;
-
-            while (counter > 0) {
-                index = Math.floor(Math.random() * counter);
-                counter--;
-                temp = array[counter];
-                array[counter] = array[index];
-                array[index] = temp;
-            }
-
-            return array;
-        };
-
         var deck = [
             'card1', 'card1', 'card2', 'card2', 'card3', 'card3', 'card4', 'card4',
             'card5', 'card5', 'card6', 'card6', 'card7', 'card7', 'card8', 'card8'
@@ -52,15 +123,18 @@
 
         shuffle(deck);
 
-        var xs = 50,
+        var xs = 40,
             x = xs,
-            y = 50,
-            offset = 90,
-            i, j;
+            y = 40,
+            offset = 110,
+            i, j, index, key, card;
 
         for (i = 0; i < this.rows; i++) {
             for (j = 0; j < this.cols; j++) {
-                this.cards.push(new Card((i * this.rows) + j, x, y, deck.pop(), this, this.game));
+                index = (i * this.rows) + j;
+                key = deck.pop();
+                new Card(index, x, y, key, this.game);
+                this.numCardsTotal++;
                 x += offset;
             }
             x = xs;
@@ -73,7 +147,7 @@
     /**
      * Renders the score text.
      */
-    Board.prototype.renderScoreText = function() {
+    Board.prototype.renderScoreText = function () {
         if (this.scoreText) {
             this.game.world.remove(this.scoreText);
         }
@@ -81,7 +155,7 @@
         var style = {font: '20px Arial', fill: '#ffffff', align: 'left'},
             text = 'Score: ' + this.score;
 
-        this.scoreText = this.game.add.text(50, this.game.height - 50, text, style);
+        this.scoreText = this.game.add.text(40, this.game.height - 50, text, style);
     };
 
     /**
@@ -89,42 +163,24 @@
      */
     Board.prototype.update = function () {
         // collect or flip cards back
-        if (this.flippedCards.length === 2 && !this.disabled) {
-            var self = this,
-                i, l;
-            this.disabled = true;
+        if (globals.flippedCards.count() === 2 && this.state === this.states.DEFAULT) {
+            this.state = this.states.LOCKED;
+            var self = this;
             setTimeout(function () {
-                if (self.compareCards(self.flippedCards[0], self.flippedCards[1])) {
-                    for (i = 0, l = self.flippedCards.length; i < l; i++) {
-                        self.flippedCards[i].collect();
-                        self.collectedCards.push(self.flippedCards[i]);
-                    }
-                    self.comboLength++;
-                    self.addPoints(100);
+                if (self.compareCards(globals.flippedCards.get(0), globals.flippedCards.get(1))) {
+                    self.collectCards(globals.flippedCards.getItems());
                 } else {
-                    for (i = 0, l = self.flippedCards.length; i < l; i++) {
-                        self.flippedCards[i].flip();
-                    }
-                    self.comboLength = 0;
+                    self.flipCardsBack(globals.flippedCards.getItems());
                 }
-                self.flippedCards = [];
-                self.disabled = false;
+                globals.flippedCards.clear();
+                self.state = self.states.DEFAULT;
             }, 2000);
         }
 
         // victory check
-        if (this.collectedCards.length === (this.rows * this.cols)) {
+        if (globals.collectedCards.length === this.numCardsTotal) {
             // game won
         }
-    };
-
-    /**
-     * Adds points to the current score.
-     * @param {number} amount
-     */
-    Board.prototype.addPoints = function(amount) {
-        this.score += amount * this.comboLength;
-        this.renderScoreText();
     };
 
     /**
@@ -133,8 +189,53 @@
      * @param {Card} b
      * @returns {boolean}
      */
-    Board.prototype.compareCards = function(a, b) {
+    Board.prototype.compareCards = function (a, b) {
         return a.key === b.key;
+    };
+
+    /**
+     * Collects the flipped cards.
+     * @param {Array} cards
+     */
+    Board.prototype.collectCards = function (cards) {
+        for (var i = 0, l = cards.length; i < l; i++) {
+            cards[i].collect();
+        }
+        this.comboLength++;
+        this.addPoints(100);
+    };
+
+    /**
+     * Flips the given cards back.
+     * @param {Array} cards
+     */
+    Board.prototype.flipCardsBack = function (cards) {
+        for (var i = 0, l = cards.length; i < l; i++) {
+            cards[i].flipBack();
+        }
+        this.comboLength = 0;
+        this.removePoints(25);
+    };
+
+    /**
+     * Adds points to the current score.
+     * @param {number} amount
+     */
+    Board.prototype.addPoints = function (amount) {
+        this.score += amount * this.comboLength;
+        this.renderScoreText();
+    };
+
+    /**
+     * Removes points from the current score.
+     * @param {number} amount
+     */
+    Board.prototype.removePoints = function (amount) {
+        this.score -= amount;
+        if (this.score < 0) {
+            this.score = 0;
+        }
+        this.renderScoreText();
     };
 
     /**
@@ -143,11 +244,16 @@
      * @param {number} x
      * @param {number} y
      * @param {number} key
-     * @param {Board} board
      * @param {Phaser.Game} game
      * @constructor
      */
-    var Card = function (index, x, y, key, board, game) {
+    var Card = function (index, x, y, key, game) {
+        this.states = {
+            DEFAULT: 'default',
+            FLIPPED: 'flipped',
+            COLLECTED: 'collected'
+        };
+
         var back = game.add.sprite(x, y, 'back');
         back.inputEnabled = true;
         back.events.onInputDown.add(this.flip, this);
@@ -155,39 +261,49 @@
         var front = game.add.sprite(x, y, key);
         front.visible = false;
 
+        var flip = game.add.sprite(x, y, 'flip');
+        flip.visible = false;
+        flip.animations.add('flip');
+
+        this.sprites = {
+            back: back,
+            front: front,
+            flip: flip
+        };
+
         this.index = index;
         this.key = key;
-        this.board = board;
-        this.front = front;
-        this.back = back;
-        this.flipped = false;
-        this.collected = false;
+        this.state = this.states.DEFAULT;
     };
 
     /**
      * Flips this card.
      */
     Card.prototype.flip = function () {
-        if (this.collected) {
-            return;
+        if (this.state === this.states.DEFAULT) {
+            if (globals.flippedCards.count() < 2) {
+                this.sprites.back.visible = false;
+                this.sprites.flip.animations.play('flip', 20);
+                this.sprites.flip.visible = true;
+                var self = this;
+                setTimeout(function () {
+                    self.sprites.flip.visible = false;
+                    self.sprites.front.visible = true;
+                    self.state = self.states.FLIPPED;
+                    globals.flippedCards.add(self);
+                }, 120);
+            }
         }
-        if (!this.flipped) {
-            if (this.board.flippedCards.length < 2) {
-                this.board.flippedCards.push(this);
-                this.front.visible = true;
-                this.back.visible = false;
-                this.flipped = true;
-            }
-        } else {
-            for (var i = 0, l = this.board.flippedCards.length; i < l; i++) {
-                if (this.board.flippedCards === this) {
-                    this.board.flippedCards.splice(i, 1);
-                    break;
-                }
-            }
-            this.front.visible = false;
-            this.back.visible = true;
-            this.flipped = false;
+    };
+
+    /**
+     * Flips this card back.
+     */
+    Card.prototype.flipBack = function () {
+        if (this.state === this.states.FLIPPED) {
+            this.sprites.front.visible = false;
+            this.sprites.back.visible = true;
+            this.state = this.states.DEFAULT;
         }
     };
 
@@ -195,19 +311,19 @@
      * Collects this card.
      */
     Card.prototype.collect = function () {
-        this.back.visible = false;
-        this.front.visible = false;
-        this.collected = true;
+        if (this.state !== this.states.COLLECTED) {
+            globals.collectedCards.add(this);
+            this.sprites.back.visible = false;
+            this.sprites.front.visible = false;
+            this.state = this.states.COLLECTED;
+        }
     };
 
-    var board;
-
     /**
-     *
+     * Performs preloading for the game.
      * @param {Phaser.Game} game
      */
     var preload = function (game) {
-        game.load.image('logo', 'img/phaser.png');
         game.load.image('back', 'img/card_back.png');
         game.load.image('card1', 'img/card_bulldozer.png');
         game.load.image('card2', 'img/card_firetruck.png');
@@ -217,25 +333,27 @@
         game.load.image('card6', 'img/card_steamboat.png');
         game.load.image('card7', 'img/card_submarine.png');
         game.load.image('card8', 'img/card_train.png');
+        game.load.spritesheet('flip', 'img/card_flip', 100, 100, 6);
     };
 
     /**
-     *
+     * Creates the game.
      * @param {Phaser.Game} game
      */
     var create = function (game) {
-        board = new Board(CARDS_PER_ROW, CARDS_PER_COLUMN, game);
+        globals.board = new Board(4, 4, game);
     };
 
     /**
-     *
+     * Updates the game.
      * @param {Phaser.Game} game
      */
     var update = function (game) {
-        board.update();
+        globals.board.update();
     };
 
-    new Phaser.Game(450, 480, Phaser.AUTO, 'memory', {
+    // Create the actual game.
+    new Phaser.Game(510, 540, Phaser.AUTO, 'memory', {
         preload: preload,
         create: create,
         update: update
