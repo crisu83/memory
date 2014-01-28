fc8d708b384532c00785c69459883922d054a327
Crisu83 <ChristofferNiska@gmail.com>
2014-01-28 02:14:12 +0200

    Rename web to src and finish the build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,26 +3,40 @@ module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
+        copy: {
+            dist: {
+                expand: true,
+                cwd: 'src/',
+                src: ['**'],
+                dest: 'dist/'
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    'dist/css/styles.css': ['src/css/styles.css']
+                }
+            }
+        },
         imagemin: {
             dist: {
                 files: [{
                     expand: true,
-                    cwd: 'img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'img/'
+                    src: ['src/img/**/*.{png,jpg,gif}'],
+                    dest: 'dist/img'
                 }]
             }
         },
         uglify: {
             dist: {
                 files: {
-                    'js/memory.min.js': ['js/memory.js']
+                    'dist/js/memory.js': ['src/js/memory.js']
                 }
             }
         }
     });
 
     grunt.registerTask('default', []);
-    grunt.registerTask('build', ['imagemin', 'uglify']);
+    grunt.registerTask('build', ['copy:dist', 'cssmin:dist', 'uglify:dist', 'imagemin:dist']);
 
 };
